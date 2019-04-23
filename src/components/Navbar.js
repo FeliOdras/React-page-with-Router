c0d714cb324f2040048380aaadcb34b0b8ddf236
FeliOdras <ariane.feldmann@odras.de>
2019-04-23 10:59:50 +0200

    Created navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className="main-navigation">
+            <ul>
+                <li>Welcome</li>
+                <li>About</li>
+                <li>Info</li>
+                <li>Credits</li>
+            </ul>
+        </nav>
+    )
+}
+
