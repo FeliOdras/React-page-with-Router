e9f21237629063e6687df8b41049a378e41e34e4
FeliOdras <ariane.feldmann@odras.de>
2019-04-23 13:17:31 +0200

    Added link to landing page on logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from '../images/logo.svg';
-import Navbar from './Navbar'
-import { BrowserRouter, Route } from 'react-router-dom'
-import Home from './Home'
-import About from './About'
-import Info from './Info'
-import Credits from './Credits'
-import Topics from './Topics'
-import Footer from './Footer'
+import Logo from './Logo';
+import Navbar from './Navbar';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Info from './Info';
+import Credits from './Credits';
+import Topics from './Topics';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -15,14 +15,14 @@ function App() {
       <BrowserRouter>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            <Logo />
             <Navbar />
-            <Route path="/" exact render={() => <h2>Welcome Stanger</h2>} />
             <Route path="/home" component={Home} exact />
             <Route path="/about" component={About} />
             <Route path="/info" component={Info} />
             <Route path="/credits" component={Credits} />
             <Route path="/topics" component={Topics} />
+            <Route path="/" exact render={() => <h2>Welcome Stanger</h2>} />
           </header>
         </div>
       </BrowserRouter>
