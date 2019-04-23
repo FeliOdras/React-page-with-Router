87be27a757ae58f7f831dde442a305a07a6d4f35
FeliOdras <ariane.feldmann@odras.de>
2019-04-23 11:19:29 +0200

    Added Link component to nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,26 @@ export default function Navbar() {
     return (
         <nav className="main-navigation">
             <ul>
-                <li>Welcome</li>
-                <li>About</li>
-                <li>Info</li>
-                <li>Credits</li>
+                <li>
+                    <Link to="/">
+                        Welcome
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about">
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/info">
+                        Info
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/credits">
+                        Credits
+                    </Link>
+                </li>
             </ul>
         </nav>
     )
