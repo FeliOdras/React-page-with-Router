a51c75773b2a339d850a6a224da13c3105e249d7
FeliOdras <ariane.feldmann@odras.de>
2019-04-23 11:35:25 +0200

    Added and imported home component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../images/logo.svg';
 import Navbar from './Navbar'
 import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './Home'
 import About from './About'
 import Info from './Info'
 import Credits from './Credits'
@@ -13,6 +14,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Navbar />
+          <Route path="/" component={Home} exact />
           <Route path="/about" component={About} />
           <Route path="/info" component={Info} />
           <Route path="/credits" component={Credits} />
