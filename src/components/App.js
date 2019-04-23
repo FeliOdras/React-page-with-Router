638702850a7e9cc52c24e5d19b6262c9a63d182f
FeliOdras <ariane.feldmann@odras.de>
2019-04-23 11:48:20 +0200

    Added footer to app

--- src/components/App.js
+++ src/components/App.js
@@ -6,22 +6,26 @@ import Home from './Home'
 import About from './About'
 import Info from './Info'
 import Credits from './Credits'
+import Footer from './Footer'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Navbar />
-          <Route path="/" exact render={() => <h2>Welcome Stanger</h2>} />
-          <Route path="/home" component={Home} exact />
-          <Route path="/about" component={About} />
-          <Route path="/info" component={Info} />
-          <Route path="/credits" component={Credits} />
-        </header>
-      </div>
-    </BrowserRouter>
+    <React.Fragment>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Navbar />
+            <Route path="/" exact render={() => <h2>Welcome Stanger</h2>} />
+            <Route path="/home" component={Home} exact />
+            <Route path="/about" component={About} />
+            <Route path="/info" component={Info} />
+            <Route path="/credits" component={Credits} />
+          </header>
+        </div>
+      </BrowserRouter>
+      <Footer />
+    </React.Fragment>
   );
 }
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="hotpink">Powered by hotpink</div>
+        </footer>
+    )
+}
+
+export default Footer
