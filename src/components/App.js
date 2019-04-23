1c945533f6096b277a38ece27ae8cd91ce269a02
FeliOdras <ariane.feldmann@odras.de>
2019-04-23 11:21:33 +0200

    Wrapped navbar in browser router

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Navbar />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Navbar />
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
