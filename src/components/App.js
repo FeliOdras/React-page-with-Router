70e17d260a18e56db72a4caa2ad033ae4674e73b
FeliOdras <ariane.feldmann@odras.de>
2019-04-23 11:01:11 +0200

    Imported navbar to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from '../images/logo.svg';
+import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Navbar />
       </header>
     </div>
   );
 }
-
 export default App;
